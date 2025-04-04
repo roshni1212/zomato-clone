ee5affe93aa83d8990e90b18c4b2183dd88727d7
roshni1212 <roshninayankara@gmail.com>
2025-04-04 20:49:20 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
 // Backend main server file
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import restaurantRoutes from "./routes/restaurantRoutes.js";
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+app.use("/api/restaurants", restaurantRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
